70892d696561fdd4fc1c119b83013c7fe95e7a78
DaviAlmeida11 <davi.almeida1713@gmail.com>
2026-04-09 01:16:49 -0300

    Mensagem do commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import imagem from "./img/SONARA LOGO.svg"; // importa a imagem
-import "./App.css";
+
+import { useState } from 'react'
+import logo from './img/SONARA LOGO.svg';
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <body>
+      
+    
       <header>
-        <img src={imagem} alt="Banana" />
+        <img src={logo} alt="logo" />
       </header>
       <main>
         <div className="mensagem">
-          <h1>BEM-VINDO A SONARA</h1>
-          <span>UNIFICANDO</span>
-          <h2> OQUE VOCE BUSCA ?</h2>
+          <h1> BEM-VINDO A SONARA</h1>
+          <p>UNIFICANDO IDEIAS, REALIZANDO SONHOS</p>
+          <h2>OQUE VOCE BUSCA ?</h2>
         </div>
+        <div className="botao">
         <button>
-          <p>Contratar Arttista</p>
+          <span>Contratar Artista</span>
         </button>
         <button>
-          <p>Fazer Shows</p>
+         <span>Fazer Shows </span> 
         </button>
         <button>
-          <p>Ver Eventos</p>
+         <span>Ver Eventos </span> 
         </button>
+       </div>
       </main>
-    </body>
-  );
+   </body>
+  )
 }
 
-export default App;
+export default App
